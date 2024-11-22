0bcedd57b1288ad8775609f6a42ddc6752324868
Sibasundar Jena <123635631+sibasundarj8@users.noreply.github.com>
2024-11-22 08:24:07 +0530

    Update and rename Longest_valid_Parentheses.java to S5_Longest_valid_Parentheses.java

--- Strings/S5_Longest_valid_Parentheses.java
+++ Strings/S5_Longest_valid_Parentheses.java
@@ -11,7 +11,7 @@ package Strings;/*
  */
 import java.util.Scanner;
 
-public class Longest_valid_Parentheses {
+public class S5_Longest_valid_Parentheses {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the String :");
