6513883e8e0efef31b8d6acc9d7367eea5df88c2
Sibasundar Jena <sibasundarj8@gmail.com>
2025-03-28 15:43:05 +0530

    Update GFG_134_Activity_Selection.java
    Approach: Idea is to first store the start and finish time such that they are inter related then sort in increasing order on the basis of finish time and check the overlapped works.
    
    TC : O(n log n)
    SC : O(n)

--- GFG_160/GFG_134_Activity_Selection.java
+++ GFG_160/GFG_134_Activity_Selection.java
@@ -10,7 +10,7 @@ package GFG_160;/*
  *      of activities that a person can complete in a day.
  *   Ex.
  *      Input : start[] = [1, 3, 0, 5, 8, 5]
- *             finish[] = [2, 4, 6, 7, 9, 9]
+ *             finish[] = [2, 4, 6, 7, 9, 9] 
  *      Output: 4
  *      Explanation: A person can perform at most four activities. The maximum set of activities that
  *                   can be executed is {0, 1, 3, 4}
