eb0d248d2f3b13ead1eb1fe7fed2f628b2c299e4
Sibasundar Jena <sibasundarj8@gmail.com>
2025-04-16 18:57:45 +0530

    Create GFG_153_Floyd_Warshall.java
    Approach : Fix a vertex and find shortest paths for all the path goes via that vertex, Do this process for every vertex.
    
    TC : O(n³)
    SC : O(1)

---
+++ GFG_160/GFG_153_Floyd_Warshall.java
@@ -0,0 +1,71 @@
+package GFG_160;/*
+ *
+ * https://www.geeksforgeeks.org/problems/implementing-floyd-warshall2042/1
+ *
+ * # Floyd Warshall
+ *
+ *   Q. You are given a weighted directed graph, represented by an adjacency matrix, dist[][]
+ *      of size n x n, where dist[i][j] represents the weight of the edge from node i to node
+ *      j. If there is no direct edge, dist[i][j] is set to a large value (i.e., 108) to represent infinity.
+ *
+ *      The graph may contain negative edge weights, but it does not contain any negative weight
+ *      cycles.
+ *
+ *      Your task is to find the shortest distance between every pair of nodes i and j in the
+ *      graph.
+ *
+ *      Note: Modify the distances for every pair in place.
+ *   Ex.
+ *      Input : dist[][] = [[0, 4, 108, 5, 108],
+ *                          [108, 0, 1, 108, 6],
+ *                          [2, 108, 0, 3, 108],
+ *                          [108, 108, 1, 0, 2],
+ *                          [1, 108, 108, 4, 0]]
+ *
+ *      Output: [[0, 4, 5, 5, 7], 
+ *               [3, 0, 1, 4, 6], 
+ *               [2, 6, 0, 3, 5], 
+ *               [3, 7, 1, 0, 2], 
+ *               [1, 5, 5, 4, 0]]
+ *
+ *      Explanation: Each cell dist[i][j] in the output shows the shortest distance from node i 
+ *                   to node j, computed by considering all possible intermediate nodes.
+ */
+public class GFG_153_Floyd_Warshall {
+
+    /// main Method
+    public static void main(String[] args) {
+        int[][] dist = {{0, 4, 108, 5, 108},
+                        {108, 0, 1, 108, 6},
+                        {2, 108, 0, 3, 108},
+                        {108, 108, 1, 0, 2},
+                        {1, 108, 108, 4, 0}};
+
+        floydWarshall(dist);
+
+        for (int[] i : dist){
+            for (int j : i){
+                System.out.print(j + "  ");
+            }
+            System.out.println();
+        }
+    }
+
+    /// Solution
+    static void floydWarshall(int[][] dist) {
+        // potd.code.hub
+        int n = dist.length;
+
+        for (int via = 0;via < n;via++){
+            for (int u = 0;u < n;u++){
+                for (int v = 0;v < n;v++){
+                    if (u == v || u == via || v == via || 
+                        dist[u][via] == 1e8 || dist[via][v] == 1e8){
+                        continue;
+                    }
+                    dist[u][v] = Math.min(dist[u][v], dist[u][via] + dist[via][v]);
+                }
+            }
+        }
+    }
+}
