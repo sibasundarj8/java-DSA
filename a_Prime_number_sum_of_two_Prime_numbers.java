4f8ed5a813ff614a6df5ea34da7a2193566215f4
Sibasundar Jena <sibasundarj8@gmail.com>
2025-05-15 21:49:20 +0530

    Update a_Prime_number_sum_of_two_Prime_numbers.java

--- a_Prime_number_sum_of_two_Prime_numbers.java
+++ a_Prime_number_sum_of_two_Prime_numbers.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 
 public class a_Prime_number_sum_of_two_Prime_numbers {
-    public a_Prime_number_sum_of_two_Prime_numbers() {
-    }
 
+    /// Solution
     public static boolean is_Prime(int n) {
         if (n <= 1) {
             return false;
@@ -17,10 +16,11 @@ public class a_Prime_number_sum_of_two_Prime_numbers {
         }
     }
 
+    /// main Method
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter number: ");
         int n = sc.nextInt();
 
         if (is_Prime(n) && is_Prime(n - 2)) {
