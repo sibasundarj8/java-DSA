ffb10f7369d6fea9e4cd6eb8ffe2867a11ed0411
Sibasundar Jena <sibasundarj8@gmail.com>
2025-05-20 19:55:46 +0530

    Update Sum_of_the_Digits.java

--- Sum_of_the_Digits.java
+++ Sum_of_the_Digits.java
@@ -1,19 +1,15 @@
 import java.util.Scanner;
 
-public class Sum_of_the_Digits
-{
-    public static void main(String[] args)
-    {
+public class Sum_of_the_Digits{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        for(int i = 1; i <= 3; ++i)
-        {
+        for(int i = 1; i <= 3; ++i){
             int n = sc.nextInt();
             int a = n;
 
             int count;
-            for(count = 0; n > 0; n /= 10)
-            {
+            for(count = 0; n > 0; n /= 10){
                 count += n % 10;
             }
 
