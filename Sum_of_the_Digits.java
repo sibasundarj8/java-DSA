3c3fbd2779222acd9c2cdb2384d330d7f13b97d7
Sibasundar Jena <sibasundarj8@gmail.com>
2025-05-23 21:31:10 +0530

    Update Sum_of_the_Digits.java

--- Sum_of_the_Digits.java
+++ Sum_of_the_Digits.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Sum_of_the_Digits{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
+            
         for(int i = 1; i <= 3; ++i){
             int n = sc.nextInt();
             int a = n;
