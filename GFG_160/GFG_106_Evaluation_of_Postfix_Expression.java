a6f5610c058fdbb9417c4876e5561f64df749ef7
Sibasundar Jena <sibasundarj8@gmail.com>
2025-09-16 21:10:03 +0530

    Update GFG_106_Evaluation_of_Postfix_Expression.java
    Approach : use a stack to store numbers until a operator being appeared. When operator come it pops two numbers from the stack and push the solved value. check explicitly for negative fractional values.
    
    TC : O(n)
    SC : O(n)

--- GFG_160/GFG_106_Evaluation_of_Postfix_Expression.java
+++ GFG_160/GFG_106_Evaluation_of_Postfix_Expression.java
@@ -4,31 +4,23 @@ package GFG_160;/*
  *
  * # Evaluation of Postfix Expression
  *
- *   Q. You are given an array of string arr that represents a valid arithmetic expression written
- *      in Reverse Polish Notation (Postfix Notation). Your task is to evaluate the expression and
- *      return an integer representing its value.
+ *   Q. You are given an array of strings arr[] that represents a valid arithmetic expression written in
+ *      Reverse Polish Notation (Postfix Notation). Your task is to evaluate the expression and return an
+ *      integer representing its value.
  *
- *      Key Details:
+ *      Note: A postfix expression is of the form operand1 operand2 operator (e.g., "a b +").
+ *            And the division operation between two integers always computes the floor value,
+ *            i.e floor(5 / 3) = 1 and floor(-5 / 3) = -2.
  *
- *      The valid operators are '+', '-', '*', and '/'.
- *
- *      Each operand is guaranteed to be a valid integer or another expression.
- *
- *      The division operation between two integers always rounds the result towards zero,
- *      discarding any fractional part.
- *
- *      No division by zero will occur in the input.
- *
- *      The input is a valid arithmetic expression in Reverse Polish Notation.
- *
- *      The result of the expression and all intermediate calculations will fit in a 32-bit
- *      signed integer.
- *    Ex.
- *      Input : arr = ["100", "200", "+", "2", "/", "5", "*", "7", "+"]
- *      Output: 757
- *      Explanation: If the expression is converted into an infix expression, it will be
- *                   ((100 + 200) / 2) * 5 + 7  = 150 * 5 + 7 = 757.
+ *            It is guaranteed that the result of the expression and all intermediate calculations will
+ *            fit in a 32-bit signed integer.
+ *      Ex.
+ *          Input : arr[] = ["2", "3", "^", "1", "+"]
+ *          Output: 9
+ *          Explanation: If the expression is converted into an infix expression, it will be
+ *                       2 ^ 3 + 1 = 8 + 1 = 9.
  */
+
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -44,14 +36,14 @@ public class GFG_106_Evaluation_of_Postfix_Expression {
         String[] postExpression = new String[n];
 
         System.out.println("Enter Expression: ");
-        for (int i = 0;i < n;i++)
+        for (int i = 0; i < n; i++)
             postExpression[i] = sc.next();
 
-        System.out.println(evaluate(postExpression));
+        System.out.println(evaluatePostfix(postExpression));
     }
 
     /// Solution
-    static int evaluate(String[] arr) {
+    static int evaluatePostfix(String[] arr) {
         // potd.code.hub
         Stack<Integer> stack = new Stack<>();
 
@@ -59,11 +51,13 @@ public class GFG_106_Evaluation_of_Postfix_Expression {
             if (!Character.isDigit(s.charAt(0)) && s.length() == 1) {
                 int y = stack.pop();
                 int x = stack.pop();
+
                 switch (s) {
                     case "+" -> stack.push(x + y);
                     case "-" -> stack.push(x - y);
                     case "*" -> stack.push(x * y);
-                    case "/" -> stack.push(x / y);
+                    case "^" -> stack.push((int) Math.pow(x, y));
+                    case "/" -> stack.push(((float) x / y < 0 && (x % y) != 0) ? x / y - 1 : x / y);
                 }
             } else stack.push(Integer.parseInt(s));
         }
