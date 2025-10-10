31ead8044755014ea3872db3b7f9b59f4ff1577b
Sibasundar Jena <sibasundarj8@gmail.com>
2025-10-10 16:15:53 +0530

    Created sum_of_two_numbers.java

--- sum_of_two_numbers.java
+++ sum_of_two_numbers.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
 
-public class sum_of_two_numbers
-{
-    public static void main(String[] args)
-    { 
+public class sum_of_two_numbers{
+    public static void main(String[] args){ 
         Scanner sc = new Scanner(System.in);
 
         System.out.println("enter a : ");
