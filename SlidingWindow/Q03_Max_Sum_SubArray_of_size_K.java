76d6d54ef3f84655513f06f1208e5e8b1cdc8e5e
Sibasundar Jena <sibasundarj8@gmail.com>
2026-01-05 15:05:21 +0530

    Refactor max sum subarray implementation
    Refactor code to improve readability and update input method.

--- SlidingWindow/Q03_Max_Sum_SubArray_of_size_K.java
+++ SlidingWindow/Q03_Max_Sum_SubArray_of_size_K.java
@@ -1,14 +1,22 @@
-/*
- *   Q. Given an array of integers Arr of size N and a number K. Return the maximum sum of a sub-array
- *      of size K.
- *   Ex:-
- *      Input:  N = 4, K = 2
- *              Arr = [100, 200, 300, 400]
- *      output: 700
+package SlidingWindow;/*
+ *
+ * https://www.geeksforgeeks.org/problems/max-sum-subarray-of-size-k5313/1
+ *
+ * # Max Sum Subarray of size K
+ *
+ *   Q. Given an array of integers arr[]  and a number k. Return the maximum sum of a subarray of size k.
+ *      Note: A subarray is a contiguous part of any given array.
+ *    Ex.
+ *      Input : arr[] = [100, 200, 300, 400], k = 2
+ *      Output: 700
+ *      Explanation: arr2 + arr3 = 700, which is maximum.
+ *
+ *  Constraints:
+ *          1 ≤ arr.size() ≤ 10⁶
+ *          1 ≤ arr[i] ≤ 10⁶
+ *          1 ≤ k ≤ arr.size()
  */
-package Sliding_Window;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Q03_Max_Sum_SubArray_of_size_K {
@@ -16,32 +24,35 @@ public class Q03_Max_Sum_SubArray_of_size_K {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Size :");
-        int n = sc.nextInt();
+        System.out.println("Enter array elements: ");
+        String[] s = sc.nextLine().split(" ");
 
-        ArrayList<Integer>arr = new ArrayList<>();
+        int n = s.length;
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(s[i]);
 
-        System.out.println("Elements :");
-        for (int i = 0;i < n;i++)
-            arr.add(sc.nextInt());
-
-        System.out.println("K :");
+        System.out.println("Enter K: ");
         int k = sc.nextInt();
 
         System.out.println("Output :");
-        System.out.println(maximumSumSubArray(k,arr,n));
+        System.out.println(maxSubarraySum(arr,k));
     }
-    static long maximumSumSubArray(int k, ArrayList<Integer> arr, int n){
-        // potd.code.hub
-        long sum = 0;
-        for (int i = 0;i < n && i < k;i++)sum += arr.get(i);
 
-        long max = sum;
-        int i = 0, j = k;
-        while (j < n) {
-            sum += arr.get(j++) - arr.get(i++);
-            max = Math.max(max,sum);
+    /// Solution
+    static int maxSubarraySum(int[] arr, int k) {
+        int n = arr.length;
+        int sum = 0;
+
+        for(int i = 0; i < k; i++)
+            sum += arr[i];
+
+        int max = sum;
+
+        for(int i = k; i < n; i++) {
+            sum += (arr[i] - arr[i - k]);
+            max = Math.max(max, sum);
         }
+
         return max;
     }
 }
