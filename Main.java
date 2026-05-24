4fda6f3c2d3fdcfc8c80eec14cb11fdc42f6333c
Sibasundar Jena <sibasundarj8@gmail.com>
2026-05-24 21:00:32 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,23 @@
-   // # Main method
+import java.util.*;
 
+/*
+ * : ⁰ ¹ ² ³ ⁴ ⁵ ⁶ ⁷ ⁸ ⁹ ⁿ ᵏ ⁱ × ⁺ ⁻ ⁼ ⁽ ⁾ ˙
+ * : ₀ ₁ ₂ ₃ ₄ ₅ ₆ ₇ ₈ ₉ ₊ ₋ ₌ ₍ ₎
+ * : ⅐ ⅑ ⅒ ⅓ ⅔ ⅕ ⅖ ⅗ ⅘ ⅙ ⅚ ⅛ ⅜ ⅝ ⅞
+ */
 public class Main {
-    public static void main(String[]args) {
-        System.out.println(sum(5, 4, 3, 2, 1));
-    }
-    static int sum (int...a){
-        int ans = 0;
-        for (int j : a) ans += j;   
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
 
-        return ans;
+        map.put("a", null);
+        map.putIfAbsent("a", "1");
+        map.putIfAbsent("a", hello("putIf"));
+        map.computeIfAbsent("a", k -> hello("computeIf"));
+        System.out.println(map.get("a"));
     }
 
+    private static String hello(String parent) {
+        System.out.println("called by : " + parent);
+        return "Hello";
+    }
 }
