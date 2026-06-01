5e49877cc02087ff89e66a36e561a541f79ad2ce
Sibasundar Jena <sibasundarj8@gmail.com>
2026-06-01 10:21:08 +0530

    Update POTD_Maximum_product_subset_of_an_array.java

--- GFG/POTD_Maximum_product_subset_of_an_array.java
+++ GFG/POTD_Maximum_product_subset_of_an_array.java
@@ -9,43 +9,55 @@ package GFG;/*
  *          Output: 24
  *          Explanation: Maximum product will be ( -1 * -2 * 4 * 3 ) = 24
  */
-import java.util.ArrayList;
+
 import java.util.Scanner;
 
 public class POTD_Maximum_product_subset_of_an_array {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
-        System.out.println("Size :");
-        int n = sc.nextInt();
-        ArrayList<Integer>arr = new ArrayList<>();
+
         System.out.println("Elements :");
-        for (int i = 0;i < n;i++)
-            arr.add(sc.nextInt());
+        String[] s = sc.nextLine().split(" ");
+
+        int n = s.length;
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(s[i]);
+        }
+
         System.out.println("Max product :");
         System.out.println(findMaxProduct(arr));
     }
-    static long findMaxProduct(ArrayList<Integer> arr) {
-        // Code here
-        int n = arr.size();
-        long prod = 1;
-        int mod = 1000000007;
-        int zero = 0;
-        int neg = 0;
-        int min = Integer.MIN_VALUE;
-        for (Integer i : arr) {
-            if (i == 0) {
-                zero++;
+
+    static int findMaxProduct(int[] arr) {
+        // potd.code.hub
+        int mod = (int) 1e9 + 7;
+        long product = 1;
+        int negCount = 0;
+        int zeroCount = 0;
+        int negMax = Integer.MIN_VALUE;
+        int n = arr.length;
+
+        if (1 == n) return arr[0];
+
+        for (int ele : arr) {
+
+            if (0 == ele) {
+                zeroCount++;
                 continue;
-            } else if (i < 0) {
-                prod *= i;
-                min = Math.max(min, i);
-                neg++;
-            } else prod *= i;
-            prod %= mod;
+            }
+
+            if (ele < 0) {
+                negMax = Math.max(negMax, ele);
+                negCount++;
+            }
+
+            product = (product * ele) % mod;
         }
-        if (arr.isEmpty())return 0;
-        if (neg == 1 && neg+zero == n)return 0;
-        if (prod < 0)prod/=min;
-        return prod;
+
+        if ((negCount == 1 && zeroCount + negCount == n) || zeroCount == n) return 0;
+        if (product < 0) return (int) (product / negMax);
+        return (int) product;
     }
 }
