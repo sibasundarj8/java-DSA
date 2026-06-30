29719bebf767137d0642b5608eb4960cefd01d4e
Sibasundar Jena <sibasundarj8@gmail.com>
2026-06-30 13:48:40 +0530

    Update LeetCode_1358_Number_of_Substrings_Containing_All_Three_Characters.java
    Approach: Your approach uses an O(N) greedy strategy. By tracking the last seen index of 'a', 'b', and 'c', you identify the starting position of the shortest valid substring ending at 'i'. The minimum index of these three determines how many valid substrings exist ending at that position.
    
    TC : O(n)
    SC : O(1)

--- LeetCode/LeetCode_1358_Number_of_Substrings_Containing_All_Three_Characters.java
+++ LeetCode/LeetCode_1358_Number_of_Substrings_Containing_All_Three_Characters.java
@@ -13,7 +13,12 @@ package LeetCode;/*
  *      Output: 3
  *      Explanation: The substrings containing at least one occurrence of the characters a, b and
  *                   c are "aaacb", "aacb" and "acb".
+ *
+ *  Constraints:
+ *      3 <= s.length <= 5 x 10⁴
+ *      s only consists of a, b or c characters.
  */
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -26,21 +31,31 @@ public class LeetCode_1358_Number_of_Substrings_Containing_All_Three_Characters 
         System.out.println("Word which should only contains 'a', 'b', 'c'.");
         String s = sc.next();
 
+        System.out.println("Number of sub-strings which contains a, b, c at least once: ");
         System.out.println(numberOfSubstrings(s));
     }
 
     /// Solution
     static int numberOfSubstrings(String s) {
-        // @ sibasundarj8@gmail.com
-        int n = s.length(), ans = 0;
-        int[] freq = new int[3];
-        Arrays.fill(freq, -1);
+        // potd.code.hub
+        int n = s.length();
+        int a = -1;
+        int b = -1;
+        int c = -1;
+        int count = 0;
 
-        for (int i = 0;i < n;i++){
-            freq[s.charAt(i) - 'a'] = i;
-            ans += 1 + Math.min(freq[0], Math.min(freq[1], freq[2]));
+        for (int i = 0; i < n; i++) {
+            char curr = s.charAt(i);
+
+            // storing the last seen of a, b, c
+            if (curr == 'a') a = i;
+            else if (curr == 'b') b = i;
+            else c = i;
+
+            // smallest valid sub-string whose last index is i
+            count += (Math.min(a, Math.min(b, c)) + 1);
         }
 
-        return ans;
+        return count;
     }
 }
