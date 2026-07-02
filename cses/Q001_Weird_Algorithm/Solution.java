0509c80a4c09ec846ec41d7a7f43b3cc82822429
Sibasundar Jena <sibasundarj8@gmail.com>
2026-07-02 19:19:21 +0530

    Create Solution.java

---
+++ cses/Q001_Weird_Algorithm/Solution.java
@@ -0,0 +1,48 @@
+package cses.Q001_Weird_Algorithm;/*
+ *
+ * https://cses.fi/problemset/task/1068
+ *
+ * # Weird Algorithm
+ *
+ *   Q. Consider an algorithm that takes as input a positive integer n. If n is even, the algorithm divides it by two,
+ *      and if n is odd, the algorithm multiplies it by three and adds one. The algorithm repeats this, until n is one.
+ *
+ *      For example, the sequence for n = 3 is as follows:
+ *              3 --> 10 --> 5 --> 16 --> 8 --> 4 --> 2 --> 1
+ *
+ *      Your task is to simulate the execution of the algorithm for a given value of n.
+ *
+ *      Input:
+ *          The only input line contains an integer n.
+ *
+ *      Output:
+ *          Print a line that contains all values of n during the algorithm.
+ *
+ *      Constraints:
+ *          1 <= n <= 10⁶
+ *
+ *      Example:
+ *          Input:
+ *              3
+ *          Output:
+ *              3 10 5 16 8 4 2 1
+ */
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        while (n != 1) {
+            System.out.print(n + " ");
+            if ((n & 1) == 1) n = n * 3L + 1;
+            else n = n >> 1;
+        }
+
+        System.out.println(1);
+    }
+}
