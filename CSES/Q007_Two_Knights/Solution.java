2f173f35c6c880523f9e714f5a22d655812d8495
Sibasundar Jena <sibasundarj8@gmail.com>
2026-07-07 12:49:21 +0530

    Create Solution.java

---
+++ CSES/Q007_Two_Knights/Solution.java
@@ -0,0 +1,20 @@
+package CSES.Q007_Two_Knights;
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine().trim());
+
+        for (long i = 1; i <= n; i++) {
+            long total_cells = i * i;
+            long total_ways = (total_cells * (total_cells - 1)) / 2;
+            long red_zone = (i >= 3) ? ((i - 2) * (i - 1) * 2) * 2 : 0;
+
+            bw.write((total_ways - red_zone) + "\n");
+        }
+
+        bw.flush();
+    }
+}
